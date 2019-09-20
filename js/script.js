db0d8158d71035f1f8892be6b60972fe9ba6d835
Abi Rana <iamyourfriend.abi@gmail.com>
2019-09-21 01:54:41 +0545

    Mobile orientation fix, players row for mobile and minor fixes

--- js/script.js
+++ js/script.js
@@ -4,12 +4,7 @@ function mobileMenu_trigger() {
 $( "#mm_trigger-open" ).on( "click", mobileMenu_trigger);
 $( "#mm_trigger-close" ).on( "click", mobileMenu_trigger);
 
-$(window).on("load resize" ,function( event ){
-    var windoHeight = $(window).height() - 345;
-    if($(window).width() < 576){
-        $(".online_players_list").height(windoHeight*.5);
-        $(".chat_box .message_list").height(windoHeight*.5);
-    }
+function responsiveFixes() {
     if($(window).width() < 992){
         $( "header .res_block" ).append( $( "footer" ) );
     }
@@ -22,8 +17,22 @@ $(window).on("load resize" ,function( event ){
     else{
         $( "body" ).prepend( $( "header" ) );
     }
+}
+$(window).on("load resize", responsiveFixes);
+
+$(window).on("load resize", function( event ) {
+    var windoHeight = $(window).height() - 345;
+    if($(window).width() < 576 && $(window).height() > $(window).width()){
+        $(".online_players_list").height(windoHeight*.5);
+        $(".chat_box .message_list").height(windoHeight*.5);
+    }
+    else{
+        $(".online_players_list").removeAttr("style");
+        $(".chat_box .message_list").removeAttr("style");
+    }
 });
 
+// Rooms Games tabs
 $("#rooms_games-tabs a").on("click", function (e) {
     e.preventDefault()
     var tab_id = $(this).attr("id");
@@ -31,10 +40,21 @@ $("#rooms_games-tabs a").on("click", function (e) {
     
     $("#rooms_games-tabs a").removeClass("active");
     $(this).addClass("active");
+    
     $(".tab-pane[aria-labelledby]").removeClass("show active");
-
+    
     tab_pane.addClass("active");
     setTimeout(function(){
         tab_pane.addClass("show")
     }, 200);
 })
+
+$(document).ready(function() {
+    $('table.table').DataTable( {
+        paging:         false,
+        searching:      false,
+        ordering:       false,
+        scrollY:        "auto",
+        "info":         false
+    } );
+} );
