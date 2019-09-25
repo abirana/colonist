218bb9eafcd53aaf45eaeb65e93c5ebc75530a23
Abi Rana <iamyourfriend.abi@gmail.com>
2019-09-25 12:00:58 +0545

    Created variables for JS and SCSS + created more responsive view for larger screen

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+var breakPoint_width_sm = 576;
+var breakPoint_width_md = 768;
+var breakPoint_width_lg = 992;
+var breakPoint_width_xl = 1200;
+
+var breakPoint_height_sm = 375;
+var breakPoint_height_md = 670;
+var breakPoint_height_lg = 876;
+var breakPoint_height_xl = 1030;
+
 function mobileMenu_trigger() {
     $(".res_block").toggleClass("active");
 }
@@ -5,16 +15,16 @@ $( "#mm_trigger-open" ).on( "click", mobileMenu_trigger);
 $( "#mm_trigger-close" ).on( "click", mobileMenu_trigger);
 
 function responsiveFixes() {
-    if($(window).width() < 992){
+    if($(window).width() < breakPoint_width_lg){
         $( "header .res_block" ).append( $( "footer" ) );
     }
-    if($(window).width() > 991){
+    if($(window).width() >= breakPoint_width_lg){
         $( "body" ).append( $( "footer" ) );
     }
-    if($(window).width() > 575 && $(window).width() < 768){
+    if($(window).width() >= breakPoint_width_sm && $(window).width() < breakPoint_width_md){
         $( ".chat_box" ).parent().prepend( $( "header" ) );
     }
-    else if($(window).width() < 992 && $(window).height() < 375){
+    else if($(window).width() < breakPoint_width_lg && $(window).height() < breakPoint_height_sm){
         $( ".chat_box" ).parent().prepend( $( "header" ) );
     }
     else{
@@ -25,7 +35,7 @@ $(window).on("load resize", responsiveFixes);
 
 $(window).on("load resize", function( event ) {
     var windoHeight = $(window).height() - 345;
-    if($(window).width() < 576 && $(window).height() > $(window).width()){
+    if($(window).width() < breakPoint_width_sm && $(window).height() > $(window).width()){
         $(".online_players_list").height(windoHeight*.5);
         $(".chat_box .message_list").height(windoHeight*.5);
     }
@@ -37,11 +47,21 @@ $(window).on("load resize", function( event ) {
 
 $(window).on("load resize", function( event ) {
     var windoHeight = $(window).height();
-    if($(window).height() > 670 && $(window).height() < 876){
+    if($(window).height() > breakPoint_height_md && $(window).height() < breakPoint_height_lg){
         $(".online_players_list").height(windoHeight-550);
         $(".chat_box .message_list").height(windoHeight-328);
         // $(".rooms_games_tabs_block .tab-content .dataTables_scrollBody").height(windoHeight-298);
-        $(".rooms_games_tabs_block .tab-content .dataTables_scrollBody").css( "height", windoHeight-428 );
+        $(".rooms_games_tabs_block .tab-content .dataTables_scrollBody").css( "height", windoHeight-428);
+    }
+    else if($(window).height() > breakPoint_height_lg && $(window).height() < breakPoint_height_xl){
+        $(".online_players_list").height(windoHeight-(550+90));
+        $(".chat_box .message_list").height(windoHeight-(328+90));
+        $(".rooms_games_tabs_block .tab-content .dataTables_scrollBody").css( "height", windoHeight-(428+90));
+    }
+    else if($(window).height() > breakPoint_height_xl){
+        $(".online_players_list").height(windoHeight-(550+264));
+        $(".chat_box .message_list").height(windoHeight-(328+264));
+        $(".rooms_games_tabs_block .tab-content .dataTables_scrollBody").css( "height", windoHeight-(428+264));
     }
     else{
         $(".online_players_list").removeAttr("style");
